0e3ac541df4e3f3c649cc3e00b38ec5f105d0482
Henry <hgrant94@gmail.com>
2020-03-12 17:29:05 -0400

    add twitter feed

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const request = require('request-promise');
+const OAuth = require('oauth')
 
 admin.initializeApp(functions.config().firebase);
 
@@ -16,4 +18,28 @@ exports.blogs = functions.https.onRequest((req, resp) => {
   }).catch(err => {
     console.log('There was an error', err);
   })
+})
+
+exports.tweets = functions.https.onRequest((req, resp) => {
+  resp.set('Access-Control-Allow-Origin', '*');
+
+  const oauth = new OAuth.OAuth(
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/access_token',
+    functions.config().twitterapi.key,
+    functions.config().twitterapi.secret,
+    '1.0A',
+    null,
+    'HMAC-SHA1'
+  )
+
+  oauth.get(
+    'https://api.twitter.com/1.1/statuses/user_timeline.json',
+    functions.config().twitterapi.mytoken,
+    functions.config().twitterapi.mysecret,
+    (err, data, res) => {
+      if(err) console.error(err);
+      resp.send(JSON.parse(data))
+    }
+  )
 })
--- src/App.js
+++ src/App.js
@@ -12,9 +12,58 @@ function App() {
     setBlogs(data);
   }
 
+  const getTweets = async () => {
+    let resp = await fetch('https://us-central1-henrygrant.cloudfunctions.net/tweets');
+    let data = await resp.json();
+    console.log(data)
+    setTweets(data);
+  }
+
+  const urlify = text => {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(urlRegex, '<a href="$1">$1</a>')
+  }
+
+  const linkifyTwitterHandles = text => {
+    const twitterHandleRegex = /(@(\w){1,15})/g
+    return text.replace(twitterHandleRegex, '<a href="$1">$1</a>')
+  }
+
+  const htmlifyTweet = text => {
+    let ret = text;
+    ret = urlify(text);
+    ret = linkifyTwitterHandles(ret);
+    return ret;
+  }
+
+  const imagesFromTweet = tweet => {
+    if(tweet.entities.media) {
+      return tweet.entities.media.map(img => (
+        <div className='imageContainer' key={img.id}>
+          <img className='image' src={img.media_url} alt={img.media_url}/>
+        </div>
+      ))
+    } else {
+      return null;
+    }
+  }
+
+  function formattedDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    let strTime = hours + ':' + minutes + ' ' + ampm;
+    return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+  }
+
   const [blogs, setBlogs] = useState([]);
+  const [tweets, setTweets] = useState([]);
   
   useEffect(() => {
+    getTweets();
     getBlogs();    
   }, [])
   
@@ -37,15 +86,37 @@ function App() {
           </a>
         </div>
       </div>
-      <div className="blogs">
-        {blogs ? blogs.map(blog => 
-          (
-            <div className="blog" key={blog.content}>
-              <div className="title">{blog.title + ' - ' + blog.date}</div>
-              <div className="content" dangerouslySetInnerHTML={{__html: blog.content}}/>
-            </div>
-          )
-        ) : null}
+      <div className="postContainer">
+        <div className="tweets">
+          {tweets ? tweets.map(tweet => 
+            (
+              <div className="tweet" key={tweet.id}>
+                <div className="header">
+                  <div className="date">{formattedDate(new Date(tweet.created_at))}</div>
+                  <div className="link">
+                    <a href="twitter.com/_chnwx">
+                      <FontAwesomeIcon icon={faTwitter}/>
+                    </a>
+                  </div>
+                </div>
+                <div className="content" dangerouslySetInnerHTML={{__html: htmlifyTweet(tweet.text)}}/>
+                {imagesFromTweet(tweet)} 
+              </div>
+            )
+            ) : null}
+        </div>
+        {/*
+        <div className="blogs">
+          {blogs ? blogs.map(blog => 
+            (
+              <div className="blog" key={blog.content}>
+                <div className="title">{blog.title + ' - ' + blog.date}</div>
+                <div className="content" dangerouslySetInnerHTML={{__html: blog.content}}/>
+              </div>
+            )
+            ) : null}
+        </div>
+        */}
       </div>
     </div>
   );
